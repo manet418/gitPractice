a1c5ca2172369f18db898ab278d3587aa1196a81
manet <maedeh7676@gmail.com>
2023-02-12 18:32:02 +0330

    stash

--- src/Main/Student.java
+++ src/Main/Student.java
@@ -2,6 +2,7 @@ package Main;
 
 public class Student {
     private String name;
+    private String lastName;
     public static void main(String[] args) {
         System.out.println("i am student");
     }
--- src/Main/main.java
+++ src/Main/main.java
@@ -3,7 +3,6 @@ package Main;
 public class main {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test 2");
-        System.out.println("test 2");
+        System.out.println("reset test");
     }
 }
