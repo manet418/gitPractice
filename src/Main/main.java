ce2e9c92a9f7a21cbc71112d1998147dfb920bf6
manet <maedeh7676@gmail.com>
2023-02-12 13:35:34 +0330

    test2

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,9 +2,7 @@ package Main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("my name is maede !!");
         System.out.println("test");
-        System.out.println("");
         System.out.println("test 2");
     }
 }
