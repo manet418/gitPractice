e48405515e67ada260e7a29fa38bc50dae960926
manet <maedeh7676@gmail.com>
2023-02-12 13:36:56 +0330

    test2

--- src/Main/main.java
+++ src/Main/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test 2");
+        System.out.println("test 2");
     }
 }
