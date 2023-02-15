c6cdb1394e0cd15de82c9f9daed9ee1ce5ce02c8
manet <maedeh7676@gmail.com>
2023-02-15 09:25:11 +0330

    add nationalCode to Student.class

--- src/Main/Student.java
+++ src/Main/Student.java
@@ -3,6 +3,7 @@ package Main;
 public class Student {
     private String name;
     private String lastName;
+    private String nationalCode;
     public static void main(String[] args) {
         System.out.println("i am student");
         System.out.println("my name is sara");
